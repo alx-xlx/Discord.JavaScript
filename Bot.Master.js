76798f0618d04c63d3cc31a261d9359b8c027f2b
alx-xlx <alexsxd7@gmail.com>
2019-04-24 17:43:46 -0700

    Bot.Master

--- Bot.Master.js
+++ Bot.Master.js
@@ -1,10 +1,9 @@
-const Discord = require('discord.js');          // Get the library
+const Discord = require();
 const client = new Discord.Client();            // Create an instance of a Discord client
 const config = require('./config.json');
 const  {Client, Attachment, RichEmbed} = require('discord.js');
 //const client = new client();
 const fs = require ('fs');
-
 client.login(config.token);            //Reads the token from config.json
 
 client.on('ready', () => {
