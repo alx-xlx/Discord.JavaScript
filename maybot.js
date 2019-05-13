b1fe57b9d45e634d784e3078d3649285ac0a6ace
alx-xlx <alexsxd7@gmail.com>
2019-05-13 23:22:17 +0530

    maybot:

--- maybot.js
+++ maybot.js
@@ -418,5 +418,15 @@ const commands = {
         });
         message.channel.sendMessage(`__**${message.guild.name}'s Music Queue:**__ Current** ${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')} \n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
     },
-    
+    'help': (message) => {
+        let tosend = ['```xl', config.prefix 
+        + 'join : "Join Voice channel of msg sender"',	tokens.prefix 
+        + 'add : "Add a valid youtube link to the queue"', tokens.prefix 
+        + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix 
+        + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix 
+        + 'pause : "pauses the music"',	tokens.prefix 
+        + 'resume : "resumes the music"', tokens.prefix 
+        + 'skip : "skips the playing song"', tokens.prefix 
+        + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
+    }
 }
