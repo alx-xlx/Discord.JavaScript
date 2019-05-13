a6c6542e39f35b9a53ebd19f3338452cba7a2b5f
alx-xlx <alexsxd7@gmail.com>
2019-05-13 23:24:26 +0530

    maybot:

--- maybot.js
+++ maybot.js
@@ -420,13 +420,15 @@ const commands = {
     },
     'help': (message) => {
         let tosend = ['```xl', config.prefix 
-        + 'join : "Join Voice channel of msg sender"',	tokens.prefix 
-        + 'add : "Add a valid youtube link to the queue"', tokens.prefix 
-        + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix 
-        + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix 
-        + 'pause : "pauses the music"',	tokens.prefix 
-        + 'resume : "resumes the music"', tokens.prefix 
-        + 'skip : "skips the playing song"', tokens.prefix 
+        + 'join : "Join Voice channel of msg sender"',	config.prefix 
+        + 'add : "Add a valid youtube link to the queue"', config.prefix 
+        + 'queue : "Shows the current queue, up to 15 songs shown."', config.prefix 
+        + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), config.prefix 
+        + 'pause : "pauses the music"',	config.prefix 
+        + 'resume : "resumes the music"', config.prefix 
+        + 'skip : "skips the playing song"', config.prefix 
         + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
-    }
+        message.channel.sendMessage(tosend.join('\n'));
+    },
+    'reboot': (message)
 }
