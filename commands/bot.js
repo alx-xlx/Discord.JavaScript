0a73c0304fdc7849004c285b76c675dcc5a03ae6
alx-xlx <alexsxd7@gmail.com>
2019-05-16 00:16:02 +0530

    commands/bot:

--- commands/bot.js
+++ commands/bot.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot, message, args) => {
     let usericon = message.author.displayAvatarURL;
     message.delete();                   
     let botembed = new Discord.RichEmbed()
-    .setDescription("**I am a Discord Bot !!**\n**I am Currently being Developed by *r4nchy* (For Fun ofc)**")
+    .setDescription(`**I am a Discord Bot !!**\n**I am Currently being Developed by *${config.meAlias}* (For Fun ofc)**`)
     .setTitle("**TesTube**")
     .setURL(`${config.bot.image}`)
     .setThumbnail(boticon)                                  //This lin prints pic in small way
     .setColor("#9052df")
-    .addField("Version", `*v0.1 (03/04/2019)*`)
+    .addField(`Version`, `*${config.Testube.version} (03/04/2019)*`)
     .addField("Created on:", bot.user.createdAt);
     message.channel.send(botembed)
     .then(msg => {
